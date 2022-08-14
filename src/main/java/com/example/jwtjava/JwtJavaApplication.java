17af387c7028e37af15878c8d55eafee4b4667b4
andersonhsporto <anderson.higo2@gmail.com>
2022-08-14 02:30:58 +0000

    add: simples authentication

--- src/main/java/com/example/jwtjava/JwtJavaApplication.java
+++ src/main/java/com/example/jwtjava/JwtJavaApplication.java
@@ -1,5 +1,10 @@
 package com.example.jwtjava;
 
+import com.example.jwtjava.entities.AppRole;
+import com.example.jwtjava.entities.AppUser;
+import com.example.jwtjava.service.UserServiceImplementation;
+import java.util.ArrayList;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,5 +15,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JwtJavaApplication {
   public static void main(String[] args) {
     SpringApplication.run(JwtJavaApplication.class, args);
+  }
+
+  CommandLineRunner run(UserServiceImplementation userService) {
+    return args -> {
+      userService.saveRole(new AppRole(null, "ROLE_USER"));
+      userService.saveRole(new AppRole(null, "ROLE_ADMIN"));
+userService.saveUser(new AppUser(null, "Anderson", "anderson@username", "123456", new ArrayList<>( null)));
+userService.saveUser(new AppUser(null, "John", "john@username", "123456", new ArrayList<>( null)));
+userService.saveUser(new AppUser(null, "Mary", "mary@username", "123456", new ArrayList<>( null)));
+
+userService.addRoleToUser("anderson@username", "ROLE_USER");
+userService.addRoleToUser("john@username", "ROLE_USER");
+userService.addRoleToUser("mary@username", "ROLE_ADMIN");
+
+    };
   }
 }
